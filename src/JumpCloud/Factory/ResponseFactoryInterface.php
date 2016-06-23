90964489b7da619664154a9604f80f97aa3cdf82
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-06-23 21:10:00 +0100

    fix: corrects a docblock

--- src/JumpCloud/Factory/ResponseFactoryInterface.php
+++ src/JumpCloud/Factory/ResponseFactoryInterface.php
@@ -3,8 +3,13 @@
 namespace JumpCloud\Factory;
 
 use GuzzleHttp\Psr7\Response;
+use JumpCloud\Response\ResponseInterface;
 
 interface ResponseFactoryInterface
 {
+    /**
+     * @param Response $response
+     * @return ResponseInterface
+     */
     public function create(Response $response);
 }
